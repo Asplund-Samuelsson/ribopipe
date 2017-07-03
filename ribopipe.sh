07103712dad3375765fc1f57e8ae9b6f501dd300
Johannes Asplund-Samuelsson <johannes.asplund.samuelsson@scilifelab.se>
2017-07-03 13:45:11 +0200

    Steps 9-13 coded; Fixed carriage return issue in sup4 script

--- ribopipe.sh
+++ ribopipe.sh
@@ -18,6 +18,7 @@ cd $OUTDIR
 # Store input filenames in array
 INPUT_FILES=(rawfastqgz/*fastq.gz)
 
+
 ################################################################################
 # Step 1: Import data to working directory
 S=1
@@ -48,7 +49,7 @@ S=2
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Concatenating fastq files per sample\e[0m\n"
+    echo -e "\n\e[94mStep $S: Concatenating fastq files per sample...\e[0m\n"
 
     # Concatenate input files
     # Iterate over all infiles
@@ -81,7 +82,7 @@ S=3
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: FastQC read quality assessment\e[0m\n"
+    echo -e "\n\e[94mStep $S: Assessing read quality with FastQC...\e[0m\n"
 
     # Calculate number of threads to use
     # Count concatenated input files
@@ -109,7 +110,7 @@ S=4
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Trim adapter sequences\e[0m\n"
+    echo -e "\n\e[94mStep $S: Trimming adapter sequences...\e[0m\n"
 
     # Store input filenames in array (specific for step 4; files from step 2)
     input_files_4=(rawfastqgz/${EXPERIMENT_NAME}.*.fastq.gz)
@@ -156,7 +157,7 @@ S=5
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Assess footprint-length distribution\e[0m\n"
+    echo -e "\n\e[94mStep $S: Assessing footprint-length distribution...\e[0m\n"
 
     # Calculate number of threads to use
     # Count concatenated input files
@@ -188,7 +189,7 @@ S=6
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Trim ends with low-quality base calls\e[0m\n"
+    echo -e "\n\e[94mStep $S: Trimming ends with low-quality base calls...\e[0m\n"
 
     # Store input filenames in array (specific for step 6; files from step 4)
     input_files_6=(cutadapt/${EXPERIMENT_NAME}.*.cutadapt.fastq.gz)
@@ -232,7 +233,7 @@ S=7
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Remove rRNA and tRNA sequences\e[0m\n"
+    echo -e "\n\e[94mStep $S: Removing rRNA and tRNA sequences...\e[0m\n"
 
     # Calculate number of threads to use
     # Use lowest number out of THREADS and bowtie_p
@@ -268,7 +269,7 @@ S=8
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Map reads to the genome\e[0m\n"
+    echo -e "\n\e[94mStep $S: Mapping reads to the genome...\e[0m\n"
 
     # Calculate number of threads to use
     # Use lowest number out of THREADS and bowtie_p
@@ -306,9 +307,33 @@ S=9
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Count reads on read-occupied positions in genome\e[0m\n"
+    echo -e "\n\e[94mStep $S: Counting reads on read-occupied positions in genome...\e[0m\n"
 
+    # Store input filenames in array (specific for step 9; files from step 8)
+    input_files_9=(mapped/${EXPERIMENT_NAME}.*.mapped.bwt1)
 
+    # Define run_count function to be used with GNU parallel application
+    run_count() {
+      # The infile name is stored in positional argument 1
+      infile=$1
+      # Extract the sample name from the infile
+      sample_name=$(echo "$infile" | rev | cut -f 1 -d \/ | rev | cut -f 2 -d \.)
+      # Construct outfile names
+      prefix="readcount/${EXPERIMENT_NAME}.${sample_name}"
+      out_p="${prefix}.readCount.p"
+      out_m="${prefix}.readCount.m"
+      # Run the read count script with the supplied options
+      $readCountScript -i $infile --min $min_length --max $max_length \
+      --outP $out_p --outM $out_m
+    }
+
+    # Export function and variables so that each subprocess can access them
+    export -f run_count
+    export readCountScript min_length max_length
+    export EXPERIMENT_NAME
+
+    # Run read count in parallel for the input files
+    parallel --no-notice --jobs $THREADS run_count ::: ${input_files_9[@]}
 
     # Report step done
     echo -e "\n\e[92mStep $S: Done.\e[0m\n"
@@ -319,16 +344,40 @@ fi
 
 
 ################################################################################
-# Step 10: Calculate total number of mapped reads
+# Step 10: Calculate total number of mapped reads (PARALLEL)
 S=10
 
 # Check starting step
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Count total number of mapped reads\e[0m\n"
+    echo -e "\n\e[94mStep $S: Counting total number of mapped reads...\e[0m\n"
 
+    # Store input filenames in array (specific for step 10; files from step 9)
+    input_files_10=(readcount/${EXPERIMENT_NAME}.*.readCount.p)
 
+    # Define run_total function to be used with GNU parallel application
+    run_total() {
+      # The "p" infile name is stored in positional argument 1
+      infile_p=$1
+      # Reconstruct the corresponding "m" infile name
+      infile_m=$(echo $infile_p | sed -e 's/\.p$/.m/')
+      # Extract the sample name from the infile
+      sample_name=$(echo "$infile_p" | rev | cut -f 1 -d \/ | rev | cut -f 2 -d \.)
+      # Construct outfile name
+      prefix="readcount/${EXPERIMENT_NAME}.${sample_name}"
+      out="${prefix}.totalNbrMappedReads"
+      # Run the total read count script with the supplied options
+      $totalNbrMappedReadsScript --inP $infile_p --inM $infile_m --out $out
+    }
+
+    # Export function and variables so that each subprocess can access them
+    export -f run_total
+    export totalNbrMappedReadsScript
+    export EXPERIMENT_NAME
+
+    # Run total count in parallel for the input files
+    parallel --no-notice --jobs $THREADS run_total ::: ${input_files_10[@]}
 
     # Report step done
     echo -e "\n\e[92mStep $S: Done.\e[0m\n"
@@ -339,16 +388,44 @@ fi
 
 
 ################################################################################
-# Step 11: Calculate RPM on read-occupied positions in genome
+# Step 11: Calculate RPM on read-occupied positions in genome (PARALLEL)
 S=11
 
 # Check starting step
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Calculate RPM on read-occupied positions in genome\e[0m\n"
+    echo -e "\n\e[94mStep $S: Calculating RPM on read-occupied positions in genome...\e[0m\n"
 
+    # Store input filenames in array (specific for step 11; files from step 9)
+    input_files_11=(readcount/${EXPERIMENT_NAME}.*.readCount.p)
 
+    # Define run_rpm function to be used with GNU parallel application
+    run_rpm() {
+      # The "p" infile name is stored in positional argument 1
+      infile_p=$1
+      # Reconstruct the corresponding "m" infile name
+      infile_m=$(echo $infile_p | sed -e 's/\.p$/.m/')
+      # Reconstruct the corresponding "total" infile name (from step 10)
+      infile_tot=$(echo $infile_p | sed -e 's/\.readCount\.p$/.totalNbrMappedReads/')
+      # Extract the sample name from the infile
+      sample_name=$(echo "$infile_p" | rev | cut -f 1 -d \/ | rev | cut -f 2 -d \.)
+      # Construct outfile names
+      prefix="RPM/${EXPERIMENT_NAME}.${sample_name}"
+      out_p="${prefix}.RPM.p"
+      out_m="${prefix}.RPM.m"
+      # Run the read count script with the supplied options
+      $RPMscript --inP $infile_p --inM $infile_m --number $infile_tot \
+      --outP $out_p --outM $out_m
+    }
+
+    # Export function and variables so that each subprocess can access them
+    export -f run_rpm
+    export RPMscript
+    export EXPERIMENT_NAME
+
+    # Run total count in parallel for the input files
+    parallel --no-notice --jobs $THREADS run_rpm ::: ${input_files_11[@]}
 
     # Report step done
     echo -e "\n\e[92mStep $S: Done.\e[0m\n"
@@ -359,16 +436,42 @@ fi
 
 
 ################################################################################
-# Step 12: Complete RPM list by assigning “0” to all unoccupied positions
+# Step 12: Complete RPM list by assigning “0” to all unoccupied positions (PARALLEL)
 S=12
 
 # Check starting step
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Complete RPM list with zeros\e[0m\n"
+    echo -e "\n\e[94mStep $S: Completing RPM list with zeros...\e[0m\n"
 
+    # Store input filenames in array (specific for step 12; files from step 11)
+    input_files_12=(RPM/${EXPERIMENT_NAME}.*.RPM.p)
 
+    # Define run_complete function to be used with GNU parallel application
+    run_complete() {
+      # The "p" infile name is stored in positional argument 1
+      infile_p=$1
+      # Reconstruct the corresponding "m" infile name
+      infile_m=$(echo $infile_p | sed -e 's/\.p$/.m/')
+      # Extract the sample name from the infile
+      sample_name=$(echo "$infile_p" | rev | cut -f 1 -d \/ | rev | cut -f 2 -d \.)
+      # Construct outfile names
+      prefix="RPM/${EXPERIMENT_NAME}.${sample_name}"
+      out_p="${prefix}.RPM0.p"
+      out_m="${prefix}.RPM0.m"
+      # Run the zero count completion script with the supplied options
+      $RPMcompleteScript --inP $infile_p --inM $infile_m \
+      --outP $out_p --outM $out_m
+    }
+
+    # Export function and variables so that each subprocess can access them
+    export -f run_complete
+    export RPMcompleteScript
+    export EXPERIMENT_NAME
+
+    # Run zero count completion in parallel for the input files
+    parallel --no-notice --jobs $THREADS run_complete ::: ${input_files_12[@]}
 
     # Report step done
     echo -e "\n\e[92mStep $S: Done.\e[0m\n"
@@ -386,9 +489,35 @@ S=13
 if [[ START_STEP -le S ]]
   then
     # Report progress
-    echo -e "\n\e[94mStep $S: Count the number of reads on every gene\e[0m\n"
+    echo -e "\n\e[94mStep $S: Counting the number of reads on every gene...\e[0m\n"
 
+    # Store input filenames in array (specific for step 12; files from step 11)
+    input_files_13=(readcount/${EXPERIMENT_NAME}.*.readCount.p)
 
+    # Define run_genes function to be used with GNU parallel application
+    run_genes() {
+      # The "p" infile name is stored in positional argument 1
+      infile_p=$1
+      # Reconstruct the corresponding "m" infile name
+      infile_m=$(echo $infile_p | sed -e 's/\.p$/.m/')
+      # Extract the sample name from the infile
+      sample_name=$(echo "$infile_p" | rev | cut -f 1 -d \/ | rev | cut -f 2 -d \.)
+      # Construct outfile names
+      prefix="readsPerGene/${EXPERIMENT_NAME}.${sample_name}"
+      out_p="${prefix}.readsPerGene.p"
+      out_m="${prefix}.readsPerGene.m"
+      # Run the reads per gene script with the supplied options
+      $readsPerGeneScript --inP $infile_p --inM $infile_m \
+      --listP $genelistP --listM $genelistM --outP $out_p --outM $out_m
+    }
+
+    # Export function and variables so that each subprocess can access them
+    export -f run_genes
+    export readsPerGeneScript genelistP genelistM
+    export EXPERIMENT_NAME
+
+    # Run reads per gene counting in parallel for the input files
+    parallel --no-notice --jobs $THREADS run_genes ::: ${input_files_13[@]}
 
     # Report step done
     echo -e "\n\e[92mStep $S: Done.\e[0m\n"
