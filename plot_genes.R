2fea6ea24c14a92b7e9dc0ce72ccb8cf22f1241c
Johannes Asplund-Samuelsson <johannes.asplund.samuelsson@scilifelab.se>
2017-08-04 14:13:04 +0200

    Added genomic range RPM plotting with gene/operon visualization

--- plot_genes.R
+++ plot_genes.R
@@ -6,12 +6,13 @@
 # Load command line arguments
 args = commandArgs(trailingOnly=T)
 indir = args[1] # A ribopipe results directory
-genes = args[2] # Gene name(s); slr1834,sll1234, or interval; 8001-9001
-outfile = args[3] # Output plot in PDF format
+genes = args[2] # Gene name(s); slr1834,sll1234, or interval; 8001:9001
+plot_type = args[3] # Plot "facets" or "operon"
+outfile = args[4] # Output plot in PDF format
 
 # TESTING
 #indir="/ssd/jan/ribprof/seqdata/2017.03.29.PPE5/"
-#genes="slr1329,sll1580,slr1908,sll1694"
+#genes="sll1577,sll1578,sll1579,sll1580,ssl3093"
 #outfile="/tmp/ribopipe_plot.pdf"
 
 ### FILENAMES, SAMPLE IDS AND STRAND IDS #######################################
@@ -19,30 +20,30 @@ outfile = args[3] # Output plot in PDF format
 # List RPM0 filenames
 rpm_files = list.files(
   path=paste(c(indir, "/RPM"), collapse=""), pattern="\\.RPM0\\.", full.names=T
-)
+  )
 
 # List readsPerGene filenames
 gene_files = list.files(
   path=paste(c(indir, "/readsPerGene"), collapse=""),
   pattern="\\.readsPerGene\\.",
   full.names=T
-)
+  )
 
 # Obtain sample and strand IDs for RPM0 files in order
 rpm_samples = unlist(
   lapply(lapply(strsplit(rpm_files, "\\."), tail, n=3L), head, n=1L)
-)
+  )
 rpm_strands = unlist(
   lapply(strsplit(rpm_files, "\\."), tail, n=1L)
-)
+  )
 
 # Obtain sample and strand IDs for readsPerGene files in order
 gene_samples = unlist(
   lapply(lapply(strsplit(gene_files, "\\."), tail, n=3L), head, n=1L)
-)
+  )
 gene_strands = unlist(
   lapply(strsplit(gene_files, "\\."), tail, n=1L)
-)
+  )
 
 
 ### LOAD DATA ##################################################################
@@ -70,53 +71,157 @@ gen = do.call("rbind", gene_data)
 colnames(gen)[1:4] = c("Name", "Start", "End", "Reads")
 
 
-### PLOT DATA ##################################################################
+### LOAD LIBRARIES #############################################################
 
 suppressMessages(library(ggplot2))
 suppressMessages(library(ggbio))
 suppressMessages(library(GenomicRanges))
+library(egg)
 
-# Plotting for individual genes
-gene_names = unlist(strsplit(genes, ","))
 
-# Extract data for only the genes of interest
-gdat = subset(gen, Name %in% gene_names)
+### DEFINE FUNCTIONS ###########################################################
 
-# Expand gene data to every position from start to end
-gdat_exp = data.frame(
-  Name=character(),
-  Position=numeric(),
-  Sample=character(),
-  Strand=character()
-)
+# Expand gene data to every position
+expand.genes = function(gdat){
+  gdat_exp = data.frame(
+    Name=character(),
+    Position=numeric(),
+    Sample=character(),
+    Strand=character()
+    )
 
-for( i in seq(1:(nrow(gdat)))){
-  gdat_new = data.frame(
-    Name=gdat[i,]$Name,
-    Position=seq(gdat[i,]$Start, gdat[i,]$End),
-    Sample=gdat[i,]$Sample,
-    Strand=gdat[i,]$Strand
-  )
-  gdat_exp = rbind(gdat_exp, gdat_new)
+  for( i in seq(1:(nrow(gdat)))){
+    gdat_new = data.frame(
+      Name=gdat[i,]$Name,
+      Position=seq(gdat[i,]$Start, gdat[i,]$End),
+      Sample=gdat[i,]$Sample,
+      Strand=gdat[i,]$Strand
+      )
+    gdat_exp = rbind(gdat_exp, gdat_new)
+  }
+
+  return(gdat_exp)
 }
 
-# Merge with RPM data
-grpm = merge(gdat_exp, rpm)
 
-# Facilitate reversing order of positions in minus strand by multiplying with -1
-grpm$Position = ifelse(grpm$Strand == "m", grpm$Position * -1, grpm$Position)
+### PLOT DATA AS FACETS ########################################################
 
-# Calculate minimum (=start) position per gene
-mins = aggregate(Position ~ Name, grpm, min)
-colnames(mins)[2] = "MinPosition"
-grpm = merge(grpm, mins)
+if (tolower(plot_type) == "facets"){
 
-# Calculate gene positions
-grpm$GenePosition = grpm$Position - grpm$MinPosition + 1
+  # Plotting for individual genes
+  gene_names = unlist(strsplit(genes, ","))
 
-gp = ggplot(grpm, aes(x=GenePosition, y=RPM, fill=Strand))
-gp = gp + geom_bar(position=position_dodge(), stat="identity")
-gp = gp + theme_bw()
-gp = gp + facet_grid(Sample~Name, scales="free_x", space="free_x")
-gp = gp + theme(axis.text.x = element_text(angle = 60, hjust=1, vjust=1))
-ggsave(outfile, gp, width=210/25.4, height=210/25.4)
+  # Extract data for only the genes of interest
+  gdat = subset(gen, Name %in% gene_names)
+
+  # Expand gene data to every position from start to end
+  gdat_exp = expand.genes(gdat)
+
+  # Merge with RPM data
+  grpm = merge(gdat_exp, rpm)
+
+  # Facilitate reversing order of positions in minus strand by multiplying with -1
+  grpm$Position = ifelse(grpm$Strand == "m", grpm$Position * -1, grpm$Position)
+
+  # Calculate minimum (=start) position per gene
+  mins = aggregate(Position ~ Name, grpm, min)
+  colnames(mins)[2] = "MinPosition"
+  grpm = merge(grpm, mins)
+
+  # Calculate gene positions
+  grpm$GenePosition = grpm$Position - grpm$MinPosition + 1
+
+  # Make Name factor for preserving user-specified order
+  grpm$Name = factor(as.character(grpm$Name), levels=gene_names)
+
+  gp = ggplot(grpm, aes(x=GenePosition, y=RPM, fill=Strand))
+  gp = gp + geom_bar(position=position_dodge(), stat="identity")
+  gp = gp + theme_bw()
+  gp = gp + facet_grid(Sample~Name, scales="free_x", space="free_x")
+  gp = gp + theme(axis.text.x = element_text(angle = 60, hjust=1, vjust=1))
+  ggsave(outfile, gp, width=210/25.4, height=210/25.4)
+
+}
+
+### PLOT GENOMIC RANGE (OPERON) ################################################
+
+if (tolower(plot_type) == "operon"){
+
+  # Check if range or set of gene names
+  if(grepl(":", genes, fixed=T)){
+    # Get genes from specified range
+    genes_full_range = as.numeric(unlist(strsplit(genes, ":")))
+    gdat = subset(gen, Start >= genes_full_range[1] & End <= genes_full_range[2])
+    gdat_exp = expand.genes(gdat)
+    grpm = merge(gdat_exp, rpm)
+  }else{
+    # Get range from specified genes
+    gene_names = unlist(strsplit(genes, ","))
+    gdat = subset(gen, Name %in% gene_names)
+    genes_full_range = c(
+      min(c(gdat$Start, gdat$End)) - 100,
+      max(c(gdat$Start, gdat$End)) + 100
+    )
+    gdat_exp = expand.genes(gdat)
+    grpm = merge(gdat_exp, rpm)
+  }
+
+  # Create GRanges object
+  gene_ranges = unique(gdat[,c("Name", "Start", "End", "Strand")])
+  gene_ranges$Middle = (gene_ranges$Start + gene_ranges$End)/2
+  gene_ranges$Y = 1
+  gene_ranges$LabelPosition = ifelse(
+    gene_ranges$Strand == "p",
+    gene_ranges$Start + 10,
+    gene_ranges$End - 10
+    )
+  gene_ranges$hjust = ifelse(gene_ranges$Strand == "p", "right", "left")
+  gr = GRanges(
+    seqnames = genes,
+    ranges = IRanges(
+      start = gene_ranges$Start,
+      width = gene_ranges$End - gene_ranges$Start + 1
+    ),
+    strand = ifelse(gene_ranges$Strand == "p", "+", "-"),
+    Name = gene_ranges$Name
+    )
+
+  # Perform plotting
+
+  # Plot RPM
+  gp = ggplot(grpm, aes(x=Position, y=RPM))
+  gp = gp + geom_bar(position=position_dodge(), stat="identity")
+  gp = gp + theme_bw()
+  gp = gp + facet_grid(Sample~.)
+  if(names(sort(table(gene_ranges$Strand),decreasing=TRUE)[1]) == "m"){
+    gp = gp + scale_x_reverse()
+  }
+  gp = gp + theme(axis.title.x=element_blank(),
+                  axis.text.x=element_blank(),
+                  axis.ticks.x=element_blank())
+  gp_rpm = gp
+
+  # Plot genes
+  gp = ggplot(gr)
+  gp = gp + geom_hline(yintercept=1)
+  gp = gp + geom_alignment(
+    range.geom = "arrowrect",
+    gap.geom = "segment",
+    length = 1
+    )
+  if(names(sort(table(gene_ranges$Strand),decreasing=TRUE)[1]) == "m"){
+    gp = gp + scale_x_reverse()
+  }
+  gp = gp + geom_text(
+    aes(x=LabelPosition, y=Y, label=Name, hjust=hjust), gene_ranges,
+    colour="white", size=2.5
+    )
+  gp = gp + theme_bw()
+  gp_genes = gp@ggplot # @ggplot FOR PLOTTING WITH REGULAR GGPLOT OBJECTS
+
+  # Align plots
+  pdf(outfile, width=210/25.4, height=140/25.4, onefile=FALSE)
+  ggarrange(gp_rpm, gp_genes, ncol=1, heights=c(20,1))
+  dev.off()
+
+}
